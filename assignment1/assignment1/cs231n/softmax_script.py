247d4ec0f25562c9f3f66802b9774ae6b4b67ebc
zhaoqike <zhaoqike@163.com>
2016-12-31 12:08:30 +0800

    实现softmax的超参数的选定

--- assignment1/assignment1/cs231n/softmax_script.py
+++ assignment1/assignment1/cs231n/softmax_script.py
@@ -117,3 +117,81 @@ print 'vectorized loss: %e computed in %fs' % (loss_vectorized, toc - tic)
 grad_difference = np.linalg.norm(grad_naive - grad_vectorized, ord='fro')
 print 'Loss difference: %f' % np.abs(loss_naive - loss_vectorized)
 print 'Gradient difference: %f' % grad_difference
+
+# Use the validation set to tune hyperparameters (regularization strength and
+# learning rate). You should experiment with different ranges for the learning
+# rates and regularization strengths; if you are careful you should be able to
+# get a classification accuracy of over 0.35 on the validation set.
+from classifiers import Softmax
+
+results = {}
+best_val = -1
+best_softmax = None
+learning_rates = [1e-7, 5e-6]
+regularization_strengths = [5e2, 1e4]
+
+
+iters = 10
+lr = np.random.random_sample((iters,))
+rg = np.random.random_sample((iters,))
+
+lr = (learning_rates[1]-learning_rates[0])*lr+learning_rates[0]
+rg = (regularization_strengths[1]-regularization_strengths[0])*rg+regularization_strengths[0]
+
+for iter in range(0, iters):
+  print 'iter', iter
+  sm = Softmax()
+
+  loss_hist = sm.train(X_train, y_train, learning_rate=lr[iter], reg=rg[iter],
+                        num_iters=1500, verbose=True)
+
+  y_train_pred = sm.predict(X_train)
+  tp = np.mean(y_train == y_train_pred)
+  y_val_pred = sm.predict(X_val)
+  vp = np.mean(y_val == y_val_pred)
+  results[(lr[iter], rg[iter])] = (tp, vp)
+
+  if vp > best_val:
+    best_val = vp
+    best_softmax = sm
+
+################################################################################
+# TODO:                                                                        #
+# Use the validation set to set the learning rate and regularization strength. #
+# This should be identical to the validation that you did for the SVM; save    #
+# the best trained softmax classifer in best_softmax.                          #
+################################################################################
+pass
+################################################################################
+#                              END OF YOUR CODE                                #
+################################################################################
+
+# Print out results.
+for lr, reg in sorted(results):
+    train_accuracy, val_accuracy = results[(lr, reg)]
+    print 'lr %e reg %e train accuracy: %f val accuracy: %f' % (
+        lr, reg, train_accuracy, val_accuracy)
+
+print 'best validation accuracy achieved during cross-validation: %f' % best_val
+
+# evaluate on test set
+# Evaluate the best softmax on test set
+y_test_pred = best_softmax.predict(X_test)
+test_accuracy = np.mean(y_test == y_test_pred)
+print 'softmax on raw pixels final test set accuracy: %f' % (test_accuracy, )
+
+# Visualize the learned weights for each class
+w = best_softmax.W[:-1, :]  # strip out the bias
+w = w.reshape(32, 32, 3, 10)
+
+w_min, w_max = np.min(w), np.max(w)
+
+classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+for i in xrange(10):
+    plt.subplot(2, 5, i + 1)
+
+    # Rescale the weights to be between 0 and 255
+    wimg = 255.0 * (w[:, :, :, i].squeeze() - w_min) / (w_max - w_min)
+    plt.imshow(wimg.astype('uint8'))
+    plt.axis('off')
+    plt.title(classes[i])
