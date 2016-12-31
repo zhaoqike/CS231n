d032de740d685ef4ccc35322efd25945db84e01e
zhaoqike <zhaoqike@163.com>
2016-12-31 12:05:40 +0800

    实现softmax的向量化的loss函数和梯度求解

--- assignment1/assignment1/cs231n/classifiers/softmax.py
+++ assignment1/assignment1/cs231n/classifiers/softmax.py
@@ -81,6 +81,20 @@ def softmax_loss_vectorized(W, X, y, reg):
   loss = 0.0
   dW = np.zeros_like(W)
 
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
+  scores = X.dot(W)
+  shift_scores = scores - np.max(scores, axis=1).reshape(-1, 1)
+  softmax_output = np.exp(shift_scores) / np.sum(np.exp(shift_scores), axis=1).reshape(-1, 1)
+  loss = -np.sum(np.log(softmax_output[range(num_train), list(y)]))
+  loss /= num_train
+  loss += 0.5 * reg * np.sum(W * W)
+
+  dS = softmax_output.copy()
+  dS[range(num_train), list(y)] += -1
+  dW = (X.T).dot(dS)
+  dW = dW / num_train + reg * W
+
   #############################################################################
   # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
   # Store the loss in loss and the gradient in dW. If you are not careful     #
